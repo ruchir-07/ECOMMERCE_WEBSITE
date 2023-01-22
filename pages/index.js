533f7a8c8da8c1275e0d5c4c496fbd6c93c72d08
ruchir-07 <99608006+ruchir-07@users.noreply.github.com>
2023-01-22 15:17:38 +0530

    Herobanner  done sanity connected

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,34 @@
 import React from 'react'
 import { Product, FooterBanner, HeroBanner } from '@/components'
-const Home = () => {
-  return (
-    <>
-      <HeroBanner />
+import { client } from '@/lib/client'
 
-      <div className='products-heading'>
-        <h2>Best Selling Products</h2>
-        <p>Speakers of many variation</p>
-      </div>
+const Home = ({ products, bannerData }) => (
+  <>
+    <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
 
-      <div className='products-container'>
-        {['Product 1', 'Product 2'].map((product) => product)}
-      </div>
+    <div className='products-heading'>
+      <h2>Best Selling Products</h2>
+      <p>Speakers of many variation</p>
+    </div>
 
-      <FooterBanner />
-    </>
-  )
+    <div className='products-container'>
+      {products?.map((product) => product.name)}
+    </div>
+
+    <FooterBanner />
+  </>
+);
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData }
+  }
 }
 
 export default Home
