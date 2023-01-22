5b9f234d62004b6d113ee5f16d80f6ecaa9a30a1
ruchir-07 <99608006+ruchir-07@users.noreply.github.com>
2023-01-22 19:06:22 +0530

    product section completed

--- components/Product.jsx
+++ components/Product.jsx
@@ -1,8 +1,24 @@
+import Link from 'next/link'
 import React from 'react'
+import { urlFor } from '@/lib/client'
 
-const Product = () => {
+const Product = ({ product: { image, name, slug, price } }) => {
   return (
-    <div>Product</div>
+    <div className=''>
+      <Link href={`/product/${slug.current}`}>
+        <div className="product-card">
+          <img 
+            src={urlFor(image && image[0])} 
+            alt="" 
+            width={250}
+            height={250}
+            className='product-image'
+          />
+          <p className="produc-name">{name}</p>
+          <p className="produc-price">${price}</p>
+        </div>
+      </Link>
+    </div>
   )
 }
 
