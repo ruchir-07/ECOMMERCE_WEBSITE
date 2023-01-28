9c06a40ab02f9d035e80ac1762c911405e3df551
ruchir-07 <99608006+ruchir-07@users.noreply.github.com>
2023-01-28 10:04:47 +0530

    cart logic implemented

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   return (
     <div className='navbar-container'>
       <p className='logo'>
-        <Link href='/'>JBL HEADPHONES</Link>
+        <Link href='/'>RB TELETRONICS </Link>
       </p>
       <button type='button' className='cart-icon' onClick={() => setShowCart(true)}>
         <AiOutlineShopping />
